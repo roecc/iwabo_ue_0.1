e21b14d8f2e84eaf123654de8429d8410f4ba972
Lee <>
2024-04-04 22:56:26 +0200

    added JSON to CSV python program for Twine to Unreal DataTable functionality.

---
+++ JSONtoCSV/JSONtoCSV.py
@@ -0,0 +1,53 @@
+import json
+import csv
+
+# Define the path to your JSON file
+file_path = 'file.json'  # Assuming the JSON file is named "file.json" and located in the same folder as your script
+
+# Open and read the JSON file
+with open(file_path, 'r') as json_file:
+    data = json.load(json_file)
+
+# Define CSV file path
+csv_file_path = 'output.csv'
+
+# Define CSV header
+csv_header = ['nodeID', 'en_line', 'link', 'speaker']
+
+# Open CSV file in write mode
+with open(csv_file_path, 'w', newline='') as csv_file:
+	writer = csv.writer(csv_file)
+
+	# Write header to CSV file
+	writer.writerow(csv_header)
+	
+	#print(data['passages'] + "\n")
+	for item in data['passages']:
+		nodeID = item['name']
+		en_line = item['text'].split('\n')[0]
+		if(item['tags'].split(',')[0] == "MA"):
+			speaker = 0
+		elif(item['tags'].split(',')[0] == "A"):
+			speaker = 1
+		elif(item['tags'].split(',')[0] == "J"):
+			speaker = 2
+		elif(item['tags'].split(',')[0] == "D"):
+			speaker = 3
+		link = ''
+		writer.writerow([nodeID, en_line, link, speaker])
+		speaker = 3
+		for option in item['links']:
+			en_line = option['linkText']
+			link = option['passageName']
+			writer.writerow([nodeID, en_line, link, speaker])
+
+	# Write data to CSV file
+	#for item in data:
+		#if isinstance(item, dict):  # Check if item is a dictionary
+		#name = item.get('name', '')
+		#tags = ','.join(item.get('tags', []))
+		#text = item.get('text', '')
+
+		#writer.writerow([name, tags, text])
+
+print("CSV file has been generated successfully.")
---
+++ JSONtoCSV/README.txt
@@ -0,0 +1,12 @@
+How to use:
+
+Install linked Twine add-on
+https://github.com/jtschoonhoven/twine-to-json
+
+Export story using add-on
+
+Copy JSON to file.json
+
+run JSONtoCSV.py
+
+import output.csv in unreal or google sheets for further editing.
